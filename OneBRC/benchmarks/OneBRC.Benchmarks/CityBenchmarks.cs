9d2032185c2a078d0f4875baee2fb95868302a77
Mark Rendle <mark@rendlelabs.com>
2025-01-27 15:04:16 +0000

    CityBenchmarks

---
+++ OneBRC/benchmarks/OneBRC.Benchmarks/CityBenchmarks.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace OneBRC.Benchmarks;
+
+public class CityBenchmarks
+{
+    private static readonly ReadOnlyMemory<byte> ThousandTxt = GetThousandTxt();
+
+    public static int Thing()
+    {
+        return ThousandTxt.Length;
+    }
+
+    private static byte[] GetThousandTxt()
+    {
+        using var s = Assembly.GetExecutingAssembly()
+            .GetManifestResourceStream("OneBRC.Benchmarks.thousand.txt")!;
+        var bytes = new byte[s.Length];
+        s.ReadExactly(bytes);
+        return bytes;
+    }
+}
