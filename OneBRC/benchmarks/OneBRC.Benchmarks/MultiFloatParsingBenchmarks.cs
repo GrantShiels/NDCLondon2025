bd4e44e5e54dc88c89c774e231278560459e1485
Mark Rendle <mark@rendlelabs.com>
2025-01-27 14:22:48 +0000

    Parsing floats

---
+++ OneBRC/benchmarks/OneBRC.Benchmarks/MultiFloatParsingBenchmarks.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace OneBRC.Benchmarks;
+
+[MemoryDiagnoser]
+public class MultiFloatParsingBenchmarks
+{
+    private const byte Semicolon = (byte)';';
+    private const byte Newline = (byte)'\n';
+    
+    private static readonly string Floats = Enumerable.Range(0, 1000)
+        .Select(_ => Random.Shared.NextSingle().ToString("0.000"))
+        .Aggregate((a, b) => a + "\n" + b);
+
+    private static readonly ReadOnlyMemory<byte> Utf8Floats = Encoding.UTF8.GetBytes(Floats);
+
+}
